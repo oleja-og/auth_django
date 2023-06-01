3e1bb88b5a3c131ffea3b2a6eb9347b294e13d58
oleja-og <oleja.og@gmail.com>
2023-06-01 14:52:22 +0300

    refact urls

--- account/urls.py
+++ account/urls.py
@@ -5,10 +5,8 @@ from . import views
 from .views import RegisterUser, index
 
 urlpatterns = [
-    # path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('login/', views.user_login, name='login'),
-    path('', index)
 ]
--- auth_django/urls.py
+++ auth_django/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from account import views
 
 
 urlpatterns = [
     path('accounts/', include('account.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index)
 ]
